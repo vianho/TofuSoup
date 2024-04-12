d2e6fec65b38331dfdf7f9e645d5df1b96e41925
silviana-hs <silviana@handshakes.com.sg>
2024-04-12 19:07:49 +0800

    chore:use github app credentials

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,13 @@
 import { TypescriptProject } from '@vianho/apidays24pj';
+import { GithubCredentials } from "projen/lib/github";
 const project = new TypescriptProject({
-  defaultReleaseBranch: 'main',
-  devDeps: ['@vianho/apidays24pj'],
-  name: 'TofuSoup',
+  defaultReleaseBranch: "main",
+  devDeps: ["@vianho/apidays24pj"],
+  name: "TofuSoup",
   projenrcTs: true,
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp(),
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
